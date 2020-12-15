0883d1000893720b87202c361eaf8c0a091c91f7
Daniel Ray <daniel.ray@quest.com>
2020-12-15 19:25:02 +1100

    The code base after the first offical competition. It worked :)

--- setup/database.sql
+++ setup/database.sql
@@ -1,3 +1,72 @@
+--
+-- Table structure for table `SETTINGS`
+--
+
+CREATE TABLE `SETTINGS` (
+  `START` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `SETTINGS`
+--
+
+-- ----------- Set the Competition Start date/time here ----------------------
+
+INSERT INTO `SETTINGS` (`START`) VALUES
+('2020-12-07 11:00:00');
+
+
+--
+-- Table structure for table `AUTH_TOKENS`
+--
+
+CREATE TABLE `AUTH_TOKENS` (
+  `ID` int(11) NOT NULL,
+  `SELECTOR` char(12) NOT NULL,
+  `VALIDATOR` char(64) NOT NULL,
+  `USER_ID` int(10) UNSIGNED NOT NULL,
+  `CREATED` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `EXPIRES` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `EMOTICON`
+--
+
+CREATE TABLE `EMOTICON` (
+  `ID` int(10) UNSIGNED NOT NULL,
+  `NAME` varchar(255) NOT NULL,
+  `IMAGE` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `EMOTICON`
+--
+
+INSERT INTO `EMOTICON` (`ID`, `NAME`, `IMAGE`) VALUES
+(1, 'Smile', 'smile.png'),
+(2, 'Shocked', 'shocked.png'),
+(3, 'Thumbs Up', 'thumbs_up.png');
+
+
+--
+-- Table structure for table `EMOTICON_JT`
+--
+
+CREATE TABLE `EMOTICON_JT` (
+  `ID` int(10) UNSIGNED NOT NULL,
+  `EMOTICON_ID` int(10) UNSIGNED DEFAULT NULL,
+  `PUSHUP_ID` int(10) UNSIGNED DEFAULT NULL,
+  `TEAM_MOVE_ID` int(10) UNSIGNED DEFAULT NULL,
+  `USER_ID` int(10) UNSIGNED DEFAULT NULL,
+  `CREATED` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+--
+-- Table structure for table `MAP`
+--
+
 CREATE TABLE `MAP` (
   `X` int(11) NOT NULL,
   `Y` int(11) NOT NULL,
@@ -7,58 +76,73 @@ CREATE TABLE `MAP` (
   `TOARRIVE` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Modify the values here for your own map*/
+--
+-- Dumping data for table `MAP`
+--
+
+-- --------------------------Modify for the map that you want----------------
+
 INSERT INTO `MAP` (`X`, `Y`, `STATUS`, `IMAGE`, `TOLEAVE`, `TOARRIVE`) VALUES
-(1, 0, 1, 'blocked.png', 0, 0),
-(2, 0, 1, 'blocked.png', 0, 0),
-(3, 0, 4, 'finish.png', 0, 0),
-(4, 0, 1, 'blocked.png', 0, 0),
-(5, 0, 1, 'blocked.png', 0, 0),
-(1, 1, 1, 'blocked.png', 0, 0),
-(2, 1, 0, 'empty.png', 1000, 0),
-(3, 1, 0, 'footpath_straight.png', 980, 0),
-(4, 1, 0, 'empty.png', 1000, 0),
-(5, 1, 1, 'blocked.png', 0, 0),
-(1, 2, 0, 'swamp.png', 920, 0),
-(2, 2, 0, 'empty.png', 900, 0),
-(3, 2, 0, 'footpath_y_down.png', 880, 0),
-(4, 2, 0, 'empty.png', 900, 0),
-(5, 2, 0, 'swamp.png', 920, 0),
-(1, 3, 0, 'empty.png', 800, 0),
-(2, 3, 0, 'footpath_topleft.png', 780, 0),
-(3, 3, 0, 'empty.png', 800, 0),
-(4, 3, 0, 'footpath_topright.png', 780, 0),
-(5, 3, 0, 'empty.png', 800, 0),
-(1, 4, 1, 'river.png', 0, 0),
-(2, 4, 0, 'bridge.png', 680, 0),
-(3, 4, 1, 'river.png', 0, 0),
-(4, 4, 0, 'bridge.png', 680, 0),
-(5, 4, 1, 'river.png', 0, 0),
-(1, 5, 0, 'empty.png', 600, 0),
-(2, 5, 0, 'footpath_straight.png', 480, 0),
-(3, 5, 0, 'empty.png', 600, 0),
-(4, 5, 0, 'footpath_straight.png', 580, 0),
-(5, 5, 0, 'empty.png', 600, 0),
-(1, 6, 0, 'swamp.png', 520, 0),
-(2, 6, 0, 'footpath_straight.png', 480, 0),
-(3, 6, 0, 'empty.png', 500, 0),
-(4, 6, 0, 'footpath_straight.png', 480, 0),
-(5, 6, 0, 'swamp.png', 520, 0),
-(1, 7, 0, 'empty.png', 400, 0),
-(2, 7, 0, 'footpath_bottomleft.png', 380, 0),
-(3, 7, 0, 'swamp.png', 420, 0),
-(4, 7, 0, 'footpath_bottomright.png', 380, 0),
-(5, 7, 0, 'empty.png', 400, 0),
-(1, 8, 1, 'blocked.png', 0, 0),
-(2, 8, 0, 'empty.png', 300, 0),
-(3, 8, 0, 'footpath_y_up.png', 280, 0),
-(4, 8, 0, 'empty.png', 300, 0),
-(5, 8, 1, 'blocked.png', 0, 0),
-(1, 9, 1, 'blocked.png', 0, 0),
-(2, 9, 1, 'blocked.png', 0, 0),
-(3, 9, 3, 'start.png', 200, 0),
-(4, 9, 1, 'blocked.png', 0, 0),
-(5, 9, 1, 'blocked.png', 0, 0);
+(1, 0, 1, '1-0.png', 0, 0),
+(2, 0, 1, '2-0.png', 0, 0),
+(3, 0, 4, '3-0.png', 0, 0),
+(4, 0, 1, '4-0.png', 0, 0),
+(5, 0, 1, '5-0.png', 0, 0),
+(1, 1, 1, '1-1.png', 0, 0),
+(2, 1, 0, '2-1.png', 400, 0),
+(3, 1, 0, '3-1.png', 400, 0),
+(4, 1, 0, '4-1.png', 400, 0),
+(5, 1, 1, '5-1.png', 0, 0),
+(1, 2, 0, '1-2.png', 370, 0),
+(2, 2, 0, '2-2.png', 350, 0),
+(3, 2, 0, '3-2.png', 570, 0),
+(4, 2, 0, '4-2.png', 350, 0),
+(5, 2, 0, '5-2.png', 370, 0),
+(1, 3, 0, '1-3.png', 340, 0),
+(2, 3, 0, '2-3.png', 540, 0),
+(3, 3, 0, '3-3.png', 320, 0),
+(4, 3, 0, '4-3.png', 540, 0),
+(5, 3, 0, '5-3.png', 340, 0),
+(1, 4, 0, '1-4.png', 310, 0),
+(2, 4, 0, '2-4.png', 290, 0),
+(3, 4, 0, '3-4.png', 510, 0),
+(4, 4, 0, '4-4.png', 290, 0),
+(5, 4, 0, '5-4.png', 310, 0),
+(1, 5, 1, '1-5.png', 0, 0),
+(2, 5, 0, '2-5.png', 260, 0),
+(3, 5, 1, '3-5.png', 0, 0),
+(4, 5, 0, '4-5.png', 260, 0),
+(5, 5, 1, '5-5.png', 0, 0),
+(1, 6, 0, '1-6.png', 250, 0),
+(2, 6, 0, '2-6.png', 230, 0),
+(3, 6, 0, '3-6.png', 450, 0),
+(4, 6, 0, '4-6.png', 230, 0),
+(5, 6, 0, '5-6.png', 250, 0),
+(1, 7, 0, '1-7.png', 220, 0),
+(2, 7, 0, '2-7.png', 420, 0),
+(3, 7, 0, '3-7.png', 200, 0),
+(4, 7, 0, '4-7.png', 420, 0),
+(5, 7, 0, '5-7.png', 220, 0),
+(1, 8, 0, '1-8.png', 190, 0),
+(2, 8, 0, '2-8.png', 170, 0),
+(3, 8, 0, '3-8.png', 390, 0),
+(4, 8, 0, '4-8.png', 170, 0),
+(5, 8, 0, '5-8.png', 190, 0),
+(1, 9, 1, '1-9.png', 0, 0),
+(2, 9, 0, '2-9.png', 160, 0),
+(3, 9, 0, '3-9.png', 160, 0),
+(4, 9, 0, '4-9.png', 160, 0),
+(5, 9, 1, '5-9.png', 0, 0),
+(1, 10, 1, '1-10.png', 0, 0),
+(2, 10, 1, '2-10.png', 0, 0),
+(3, 10, 3, '3-10.png', 130, 0),
+(4, 10, 1, '4-10.png', 0, 0),
+(5, 10, 1, '5-10.png', 0, 0);
+
+
+--
+-- Table structure for table `PUSHUP`
+--
 
 CREATE TABLE `PUSHUP` (
   `ID` int(11) UNSIGNED NOT NULL,
@@ -69,10 +153,10 @@ CREATE TABLE `PUSHUP` (
   `WALL` int(11) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*set a single entry for competition start date/time*/
-CREATE TABLE `SETTINGS` (
-  `START` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `TEAM`
+--
 
 CREATE TABLE `TEAM` (
   `ID` int(11) UNSIGNED NOT NULL,
@@ -84,14 +168,10 @@ CREATE TABLE `TEAM` (
   `POINTS` float DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Default teams, only need 2 teams to work.*/
-/*Set captain as a user_id for each team*/
-INSERT INTO `TEAM` (`ID`, `NAME`, `CAPTAIN`, `FLAG`, `START_X`, `START_Y`, `POINTS`) VALUES
-(1, 'Rabbit', 0, 'rabbit.png', 3, 9, 0),
-(2, 'kangaroo', 0, 'kangaroo.png', 3, 9, 0),
-(3, 'Bear', 0, 'bear.png', 3, 9, 0),
-(4, 'Penguin', 0, 'penguin.png', 3, 9, 0),
-(5, 'Turtle', 0, 'turtle.png', 3, 9, 0);
+
+--
+-- Table structure for table `TEAM_MOVE`
+--
 
 CREATE TABLE `TEAM_MOVE` (
   `ID` int(10) UNSIGNED NOT NULL,
@@ -102,7 +182,10 @@ CREATE TABLE `TEAM_MOVE` (
   `DATETIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Entries will be created on user sign up.  Need to chagne TEAM_ID to have a user put into a team*/
+--
+-- Table structure for table `USER`
+--
+
 CREATE TABLE `USER` (
   `ID` int(11) UNSIGNED NOT NULL,
   `NAME` varchar(255) DEFAULT NULL,
@@ -110,17 +193,49 @@ CREATE TABLE `USER` (
   `PASSWORD` varchar(255) DEFAULT NULL,
   `TEAM_ID` int(11) UNSIGNED DEFAULT NULL,
   `SPONSER` varchar(255) DEFAULT NULL,
-  `PRIVACY` tinyint(3) UNSIGNED NOT NULL DEFAULT '0'
+  `PRIVACY` tinyint(3) UNSIGNED NOT NULL DEFAULT '0',
+  `PUSHUP_ESTIMATE` int(11) DEFAULT NULL,
+  `PUSHUP_ESTIMATE_TYPE` char(4) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Indexes for table `AUTH_TOKENS`
+--
+ALTER TABLE `AUTH_TOKENS`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `EMOTICON`
+--
+ALTER TABLE `EMOTICON`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `EMOTICON_JT`
+--
+ALTER TABLE `EMOTICON_JT`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `PUSHUP`
+--
 ALTER TABLE `PUSHUP`
   ADD PRIMARY KEY (`ID`);
 
+--
+-- Indexes for table `TEAM`
+--
 ALTER TABLE `TEAM`
   ADD PRIMARY KEY (`ID`);
 
+--
+-- Indexes for table `TEAM_MOVE`
+--
 ALTER TABLE `TEAM_MOVE`
   ADD PRIMARY KEY (`ID`);
 
+--
+-- Indexes for table `USER`
+--
 ALTER TABLE `USER`
   ADD PRIMARY KEY (`ID`);
